1a14f9067aab0ee65a0994d95a13379dab6c861b
Francisco Cordoba <pako.cordoba10@gmail.com>
2022-08-22 18:57:57 -0600

    Site simplified

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+const http = require('http'); // or 'https' for https:// URLs
+const fs = require('fs');
 
 var app = express();
 
@@ -8,7 +10,13 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-  console.log(`✅ App listening at http://localhost:${port}`)
+    console.log(`✅ App listening at http://localhost:${port}`)
 })
+
+// const file = fs.createWriteStream("file.jpg");
+// const request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+//     console.log(response);
+//     // response.pipe(file);
+// });
 
 module.exports = app;
